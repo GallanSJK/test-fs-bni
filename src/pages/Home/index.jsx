da67e8f6a1927cd1a553184b3069fe88b249c2ac
GallanSJK <gallansteffan99@gmail.com>
2023-11-08 14:15:57 +0700

    add payment and fleet car

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,12 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 import { Autoplay, Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Tes from '../../assets/tes.png'
+import Dompet from '../../assets/dompet.png'
+import Mobil from '../../assets/mobil.png'
 import 'swiper/css';
 import 'swiper/css/pagination';
 
@@ -11,9 +14,9 @@ export default function Home() {
     return (
         <>
             <section className="Home">
-                <Container className='px-0'>
+                <Container>
                     <Row>
-                        <Col xs={12} lg={12}>
+                        <Col xs={12} lg={12} className='px-0'>
                         <div className="carousel-wrapper">
                             <div className="swiper-container">
                         
@@ -53,6 +56,47 @@ export default function Home() {
                         </Col>
                     </Row>
                 </Container>
+
+                <Container>
+                    <Row className="justify-content-center">
+                        <Col lg={12}>
+                        <div className="shadow p-3 rounded">
+                            <Row className="row-cols-lg-auto g-0">
+                                <Col xs={2} lg={2} className='px-1 d-flex align-items-center'>
+                                    <img className='img-fluid' src={Dompet} />
+                                </Col>
+                                <Col xs={10} lg={10} className='px-3'>
+                                    <h5>Rp. 50.000</h5>
+                                    <h5>Tap to change</h5>
+                                </Col>
+                            </Row>
+                        </div>
+                        </Col>
+                    </Row>
+                </Container>
+
+                <Container>
+                    <Row className='pt-3 text-center'>
+                        <Col xs={3} lg={3}>
+                            <Image className='img-fluid' src={Mobil} />
+                            <h5>Ride</h5>
+                        </Col>
+                        <Col xs={3} lg={3}>
+                            <Image className='img-fluid' src={Mobil} />
+                            <h5>Rent</h5>
+                        </Col>
+                        <Col xs={3} lg={3}>
+                            <Image className='img-fluid' src={Mobil} />
+                            <h5>Delivery</h5>
+                        </Col>
+                        <Col xs={3} lg={3}>
+                            <Image className='img-fluid' src={Mobil} />
+                            <h5>Shuttle</h5>
+                        </Col>
+                    </Row>
+                </Container>
+
+                
             </section>                
         </>
     )
